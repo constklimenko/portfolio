380d954096b93c97cc96fce950a9a2703ad418ac
narcom <narcomacer@gmail.com>
2020-05-17 17:15:47 +0500

    добавлена информация

--- gulpfile.js
+++ gulpfile.js
@@ -120,7 +120,7 @@ gulp.task('less', function () {
         .pipe(concat(config.output.cssName))
         .pipe(authoprefixer())
         .pipe(cleanCss())
-        .pipe(sourcemaps.write())
+        // .pipe(sourcemaps.write())
         .pipe(gulp.dest(config.output.path))
         .pipe(browserSync.stream());
 });
