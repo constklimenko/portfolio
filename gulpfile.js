57aa7e230a837d17607823a984e8274cb3d18ec7
narcom <narcomacer@gmail.com>
2020-06-26 10:23:14 +0500

    добавлен шаблон темы вордпресс

--- gulpfile.js
+++ gulpfile.js
@@ -19,65 +19,65 @@ const pathName = 'avto';
 
 
 const config = {
-  path: {
-    less: `${pathName}/src/less/*.less`,
-    less2: `${pathName}/src/less/parts/*.less`,
-    html: `${pathName}/public/index.html`,
+    path: {
+        less: `${pathName}/src/less/*.less`,
+        less2: `${pathName}/src/less/parts/*.less`,
+        html: `${pathName}/public/index.html`,
 
-  },
-  output: {
-    cssName: `${pathName}.css`,
-    path: `${pathName}/public`,
-    path_file: `${pathName}/public/index.html`,
-    path_file_css: `${pathName}/public/${pathName}.css`,
-    newHtml: `/tmp/fz3temp-2`
-  }
+    },
+    output: {
+        cssName: `${pathName}.css`,
+        path: `${pathName}/public`,
+        path_file: `${pathName}/public/index.html`,
+        path_file_css: `${pathName}/public/${pathName}.css`,
+        newHtml: `/tmp/fz3temp-2`
+    }
 }
 
 
 
-gulp.task('less', function () {
-  return gulp.src(config.path.less)
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(concat(config.output.cssName))
-    .pipe(authoprefixer())
-    .pipe(cleanCss())
-    // .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.output.path))
-    .pipe(browserSync.stream());
+gulp.task('less', function() {
+    return gulp.src(config.path.less)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat(config.output.cssName))
+        .pipe(authoprefixer())
+        .pipe(cleanCss())
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest(config.output.path))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('push', function () {
-  return gulp.src(config.output.path_file).pipe(gulp.dest(config.output.newHtml));
+gulp.task('push', function() {
+    return gulp.src(config.output.path_file).pipe(gulp.dest(config.output.newHtml));
 });
 
-gulp.task('pushCss', function () {
-  return gulp.src(config.output.path_file_css).pipe(gulp.dest(config.output.newHtml));
+gulp.task('pushCss', function() {
+    return gulp.src(config.output.path_file_css).pipe(gulp.dest(config.output.newHtml));
 })
 
 gulp.task('serve', (done) => {
-  browserSync.init({
-    server: {
-      baseDir: config.output.path
-    }
-  });
-  gulp.watch(config.path.less, gulp.series('less'));
-  gulp.watch(config.path.less2, gulp.series('less')); //, 'push', 'pushCss'));
-  gulp.watch(config.path.html).on('change', () => {
-    browserSync.reload();
-    done();
-  });
+    browserSync.init({
+        server: {
+            baseDir: config.output.path
+        }
+    });
+    gulp.watch(config.path.less, gulp.series('less'));
+    gulp.watch(config.path.less2, gulp.series('less'));
+    //, 'push', 'pushCss'));
+    gulp.watch(config.path.html).on('change', () => {
+        browserSync.reload();
+        done();
+    });
 
 });
 
 
 const globs = [
-  './app/index.html',
+    './app/index.html',
 
 ];
 
 
 
 gulp.task('default', gulp.series('less', 'serve'));
-
